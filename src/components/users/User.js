f6f28cbfdc059456c02c4ec28a36e9e548fa4330
mrkumar82 <mrkumar82@gmail.com>
2021-09-13 16:51:41 +0530

    updated link updated link

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 
 function User({ user }) {
   return (
-    <div>
-      <Link to={`/user-detail/${user.id}`}>
-        <img src={user.avatar} alt={user.first_name} />
-        <h4>
-          {user.first_name} {user.last_name}
-        </h4>
-        <h5>Email : {user.email}</h5>
-      </Link>
+    <div className="row">
+      <div className="col s12 m7">
+        <div className="card">
+          <div className="card-image">
+            <img src={user.avatar} alt={user.first_name} />
+          </div>
+          <div className="card-content">
+            <h4>
+              {user.first_name} {user.last_name}
+            </h4>
+            <h5>Email : {user.email}</h5>
+          </div>
+          <div className="card-action">
+            <Link to={`/user-detail/${user.id}`}>Detail</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/users/UserDetail.js
+++ src/components/users/UserDetail.js
@@ -24,12 +24,20 @@ function UserDetail() {
   return (
     <div>
       {loading && (
-        <div>
-          <img src={user.avatar} alt=" {user.first_name}" />
-          <h4>
-            {user.first_name} {user.last_name}
-          </h4>
-          <p>Email : {user.email} </p>
+        <div className="row">
+          {/* <div className="col s12 m7">
+            <div className="card">
+              <div className="card-image">
+                <img src={user.avatar} alt={user.first_name} />
+              </div>
+              <div className="card-content">
+                <h4>
+                  {user.first_name} {user.last_name}
+                </h4>
+                <h5>Email : {user.email}</h5>
+              </div>
+            </div>
+          </div> */}
         </div>
       )}
     </div>
